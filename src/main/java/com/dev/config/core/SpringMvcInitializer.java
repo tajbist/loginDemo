c3b1fee716ae420019504faf09fac0075aba5e15
Taj Bist <tajbist2@gmail.com>
2017-02-23 23:59:29 -0600

    adding new spring login demo

---
+++ src/main/java/com/dev/config/core/SpringMvcInitializer.java
@@ -0,0 +1,24 @@
+package com.dev.config.core;
+
+
+import com.dev.config.AppConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+}
---
+++ src/main/java/com/dev/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.dev.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
